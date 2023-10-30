8808d4fbc98fa2b998414ff61c1fa939096190bf
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2023-10-30 14:53:51 +0300

    Finished the Sort content

--- src/DStest01/Sort.java
+++ src/DStest01/Sort.java
@@ -1,5 +1,7 @@
 package DStest01;
 
+import java.util.Arrays;
+
 public class Sort {
 	public static void main(String [] args) {
 		//Selection Sort
@@ -12,10 +14,31 @@ public class Sort {
 		}
 		
 		System.out.println();
-		//
-		insertionSort(array);
+		
+		//InsertionSort
+		
+		//insertionSort(array);
 		for(int i : array)
 			System.out.print(i + " ");
+		
+		System.out.println();
+		
+		// Bubble Sort
+		
+		//bubbleSort(array);
+		
+		for(int i : array)
+			System.out.print(i + " ");
+		
+		System.out.println();
+		
+		mergeSort(array);
+		
+		for(int i : array)
+			System.out.print(i + " ");
+		
+		
+		
 		
 		
 		
@@ -62,6 +85,80 @@ public class Sort {
 		 			theArray[loc] = nextItem;
 		 }
 	 }
+	
+	//Bubble Sort Kısmı Burada
+	
+	// Dizi sıralı ve sırasız olarak iki kısma ayrılır || En büyük eleman bir sola doğru hareket  
+	// n elemanlık bir dizi için, bubble sort diziyi sıralamak için maksimum n-1 geçişe ihtiyaç duyar || 
+	// En kotü durumda iç dongü n -1 defa çalışır, yer değiştirme sayısı 3*n*(n-1)/2 / Karşılaştırma sayısı n*(n-1)/2 == O(n^2)
+	// En iyi durumda zaten artan sırada dizili, yer değiştirme 0, karşılaştırma n-1 == O(n)
+	
+	public static void bubbleSort(int[] theArray) {
+	    boolean sorted = false;
+	    int n = theArray.length;
+
+	    for (int pass = 1; pass < n && !sorted; pass++) {
+	        sorted = true;
+
+	        for (int index = 0; index < n - pass; index++) { // n-pass'a kadar olan elemanlar üzerinde işlem yapılmalı
+	            int nextIndex = index + 1;
+
+	            if (theArray[index] > theArray[nextIndex]) {
+	                int temp = theArray[index];
+	                theArray[index] = theArray[nextIndex];
+	                theArray[nextIndex] = temp;
+	                sorted = false;
+	            }
+	        }
+	    }
+	}
+	
+	//Merge Sort kısmı burada
+	
+	// Diğeri QuickSort || Bol ve Fethet || Diziyi iki parçaya bol, her bir parçayı kendi içinde diz, sıralanmış parçaları tek bir sıralı diziye birleştir. 
+	// Yineleme alt dizilerin uzunluğu 1 olunca dibe vurmuş olur.
+	// Yineleme bağıntısı T(N) = 2T (N/2) + O(N),, toplam O(nlgn)
+	// m terim için || 
+	// En kotü durumda kar
+	
+	public static void mergeSort(int[] array) {
+        if (array.length > 1) {
+            int mid = array.length / 2;
+            int[] leftArray = Arrays.copyOfRange(array, 0, mid);
+            int[] rightArray = Arrays.copyOfRange(array, mid, array.length);
+            mergeSort(leftArray);
+            mergeSort(rightArray);
+            merge(array, leftArray, rightArray);
+        }
+    }
+
+    private static void merge(int[] array, int[] leftArray, int[] rightArray) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < leftArray.length && j < rightArray.length) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < leftArray.length) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < rightArray.length) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+	
 
 }
 
